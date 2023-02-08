e4144d4f525efe533a977ed7a19ecbc014ce2d6e
Łukasz Adamczyk <l.adamczyk@softkraft.co>
2023-02-08 22:33:05 +0100

    [#184437151] changes related to login, registration and callback_url

--- config.py
+++ config.py
@@ -2,11 +2,12 @@ from kinde_sdk.kinde_api_client import GrantType
 
 
 SITE_HOST = "localhost"
-SITE_PORT = "8080"
+SITE_PORT = "5000"
 SITE_URL = f"http://{SITE_HOST}:{SITE_PORT}"
 LOGOUT_REDIRECT_URL = f"http://{SITE_HOST}:{SITE_PORT}"
+KINDE_CALLBACK_URL = f"http://{SITE_HOST}:{SITE_PORT}/api/auth/kinde_callback"
 CLIENT_ID = "CLIENT_ID"
 CLIENT_SECRET = "SOME_SECRET"
 KINDE_ISSUER_URL = "https://[your_subdomain].kinde.com"
-GRANT_TYPE = GrantType.AUTORIZATION_CODE
+GRANT_TYPE = GrantType.AUTHORIZATION_CODE
 CODE_VERIFIER = "CODE_VERIFIER"
